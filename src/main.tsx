1f61b55c77b8265f066344e0708cf809d7cb4a28
benwu25 <soggysocks206@gmail.com>
2025-09-13 12:09:25 -0700

    try memory router

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
-import { BrowserRouter, Routes, Route } from 'react-router'
+import { MemoryRouter, Route, Routes } from 'react-router'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import Contributions from './Contributions.tsx'
 
 createRoot(document.getElementById('root')!).render(
-<BrowserRouter>
+<MemoryRouter>
   <Routes>
     <Route path="/" element={<App />} />
     <Route path="/contributions" element={<Contributions />} />
   </Routes>
-</BrowserRouter>
+</MemoryRouter>
 )
