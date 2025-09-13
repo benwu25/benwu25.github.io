a1e4bfb585eba82f1522128cc2935fff605ecc46
benwu25 <soggysocks206@gmail.com>
2025-09-13 12:15:06 -0700

    try 404.html hack

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
-import { MemoryRouter, Route, Routes } from 'react-router'
+import { BrowserRouter, Route, Routes } from 'react-router'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import Contributions from './Contributions.tsx'
 
 createRoot(document.getElementById('root')!).render(
-<MemoryRouter>
+<BrowserRouter>
   <Routes>
     <Route path="/" element={<App />} />
     <Route path="/contributions" element={<Contributions />} />
   </Routes>
-</MemoryRouter>
+</BrowserRouter>
 )
