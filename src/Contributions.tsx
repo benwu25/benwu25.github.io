aacc3ebc708e29c65e6a89d3fb87b2109f23904f
benwu25 <soggysocks206@gmail.com>
2025-09-14 13:05:09 -0700

    adjust mobile

--- src/Contributions.tsx
+++ src/Contributions.tsx
@@ -10,10 +10,10 @@ export default function Contributions() {
         <br></br>
 
         <div>
-          <b className="text-[20px] ml-[5%]">LLVM</b>
+          <b className="text-[20px] ml-[2%]">LLVM</b>
         </div>
 
-          <ul className="list-disc ml-[12%]">
+          <ul className="list-disc ml-[9%]">
 
             <li>
               <a className="underline text-[10px]" href="https://github.com/llvm/llvm-project/pull/156962">[clang] Fix typo in comment</a>
@@ -33,10 +33,10 @@ export default function Contributions() {
         <br></br>
 
         <div>
-          <b className="text-[20px] ml-[5%]">GCC</b>
+          <b className="text-[20px] ml-[2%]">GCC</b>
         </div>
 
-          <ul className="list-disc list-outside ml-[12%]">
+          <ul className="list-disc list-outside ml-[9%]">
   
             <li>
               <a className="underline text-[10px]" href="https://gcc.gnu.org/git/?p=gcc.git;a=commitdiff;h=356250630abd876ae592bc3d2b4cc171bc834b79">middle end: Fix typo in gimple.h</a>
