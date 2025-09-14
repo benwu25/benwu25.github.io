e6807679e955c4fb247cc454c49d58c11a0813f9
benwu25 <soggysocks206@gmail.com>
2025-09-14 12:53:03 -0700

    adjust mobile

--- src/App.tsx
+++ src/App.tsx
@@ -15,17 +15,17 @@ export default function App() {
         <br></br>
 
         <div>
-          <b className="ml-[6.65%] text-[30px]">Ben Wu</b>
+          <b className="ml-[5%] text-[30px]">Ben Wu</b>
         </div>
 
         <div>
-          <a className="underline ml-[6.85%] text-[12px]" href="mailto:bwu25@cs.washington.edu">Email</a>
+          <a className="underline ml-[5.2%] text-[12px]" href="mailto:bwu25@cs.washington.edu">Email</a>
         </div>
         <div>
-          <a className="underline ml-[6.85%] text-[12px]" href="https://www.linkedin.com/in/benwu25">LinkedIn</a>
+          <a className="underline ml-[5.2%] text-[12px]" href="https://www.linkedin.com/in/benwu25">LinkedIn</a>
         </div>
         <div>
-          <a className="underline ml-[6.85%] text-[12px]" href="https://github.com/benwu25">GitHub</a>
+          <a className="underline ml-[5.2%] text-[12px]" href="https://github.com/benwu25">GitHub</a>
         </div>
 
         <br></br>
@@ -33,16 +33,16 @@ export default function App() {
         <br></br>
 
         <div>
-          <b className="text-[15px] ml-[7.5%]">About:</b>
+          <b className="text-[15px] ml-[5.85%]">About:</b>
         </div>
 
-        <div className="max-w-180 text-[10px] ml-[7.5%]">
+        <div className="max-w-180 text-[10px] ml-[5.85%]">
           Hi, I'm Ben. I am an undergraduate student at the University of Washington Seattle campus studying computer engineering in the Paul G. Allen School.
         </div>
 
         <br></br>
 
-        <div className="max-w-180 text-[10px] ml-[7.5%]">
+        <div className="max-w-180 text-[10px] ml-[5.85%]">
           Outside of school, I enjoy building, reading, using, and contributing to open source software. I am particularly interested in compiler toolchains, and I am spending a lot of my time learning and hacking inside of the LLVM project and the GNU Compiler Collection.
         </div>
 
@@ -50,7 +50,7 @@ export default function App() {
         <br></br>
 
         <div>
-          <Link className="underline ml-[7.5%] text-[12px]" to="/contributions">Open source contributions</Link>
+          <Link className="underline ml-[5.85%] text-[12px]" to="/contributions">Open source contributions</Link>
         </div>
 
         <br></br>
