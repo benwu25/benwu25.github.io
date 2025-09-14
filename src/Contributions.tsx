73011a8e596368f88fe6b9e32885c96217ed7942
benwu25 <soggysocks206@gmail.com>
2025-09-13 21:11:27 -0700

    try ml on elements

--- src/Contributions.tsx
+++ src/Contributions.tsx
@@ -12,13 +12,13 @@ export default function Contributions() {
         <b className="text-[20px] ml-[3.365%]">LLVM</b>
       </div>
 
-      <ul className="list-disc list-outside list-inside ml-[4.8%]">
+      <ul className="list-disc list-outside">
 
-        <li>
+        <li className="ml-[4.8%]">
           <a className="underline" href="https://github.com/llvm/llvm-project/pull/156962">[clang] Fix typo in comment</a>
         </li>
 
-        <li>
+        <li className="ml-[4.8%]">
           <a className="underline" href="https://github.com/llvm/llvm-project/pull/157782">[InstCombine] Fold min(X+1, Y) - min(X, Y) --&gt; zext X &lt; Y</a>
           <ul className="list-disc ml-6">
             <li>
@@ -35,17 +35,17 @@ export default function Contributions() {
         <b className="text-[20px] ml-[3.365%]">GCC</b>
       </div>
 
-      <ul className="list-disc list-outside ml-[4.8%]">
+      <ul className="list-disc list-outside">
 
-        <li>
+        <li className="ml-[4.8%]">
           <a className="underline" href="https://gcc.gnu.org/git/?p=gcc.git;a=commitdiff;h=356250630abd876ae592bc3d2b4cc171bc834b79">middle end: Fix typo in gimple.h</a>
         </li>
 
-        <li>
+        <li className="ml-[4.8%]">
           <a className="underline" href="https://gcc.gnu.org/git/?p=gcc.git;a=commitdiff;h=236633fd74706f11044ea51306775a43c294da57">c++: Fix typo in comment</a>
         </li>
 
-        <li>
+        <li className="ml-[4.8%]">
           <a className="-indent-[5%] underline" href="https://gcc.gnu.org/git/?p=gcc.git;a=commitdiff;h=54bf72ebfe983c611673bcc4368b5834773fcc7d">c++: Fix ICE on mangling invalid compound requirement</a>
           <ul className="list-disc ml-6">
             <li>
