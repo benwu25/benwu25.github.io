57fc8f73e84b95d399c4ee3b4df83e5c1ca493e6
benwu25 <soggysocks206@gmail.com>
2025-09-23 18:30:22 -0700

    add libstdc++ commit, use forge mirror repository for links

--- src/Contributions.tsx
+++ src/Contributions.tsx
@@ -42,12 +42,12 @@ export default function Contributions() {
               <a className="underline text-[10px]" href="https://gcc.gnu.org/git/?p=gcc.git;a=commitdiff;h=356250630abd876ae592bc3d2b4cc171bc834b79">middle end: Fix typo in gimple.h</a>
             </li>
   
-            <li className="">
+            <li>
               <a className="underline text-[10px]" href="https://gcc.gnu.org/git/?p=gcc.git;a=commitdiff;h=236633fd74706f11044ea51306775a43c294da57">c++: Fix typo in comment</a>
             </li>
   
-            <li className="">
-              <a className="underline text-[10px]" href="https://gcc.gnu.org/git/?p=gcc.git;a=commitdiff;h=54bf72ebfe983c611673bcc4368b5834773fcc7d">c++: Fix ICE on mangling invalid compound requirement</a>
+            <li>
+              <a className="underline text-[10px]" href="https://forge.sourceware.org/gcc/gcc-mirror/commit/54bf72ebfe983c611673bcc4368b5834773fcc7d">c++: Fix ICE on mangling invalid compound requirement</a>
             </li>
 
             <ul className="list-disc ml-6">
@@ -55,6 +55,11 @@ export default function Contributions() {
                 <a className="underline text-[10px]" href="https://gcc.gnu.org/bugzilla/show_bug.cgi?id=120618">Resolved bug 120618</a>
               </li>
             </ul>
+
+            <li>
+              <a className="underline text-[10px]" href="https://forge.sourceware.org/gcc/gcc-mirror/commit/52d702d72afa0ade8edfff144b45539495e4d408">libstdc++: fix element construction in std::deque::emplace</a>
+            </li>
+
           </ul>
 
         <br></br>
@@ -110,7 +115,7 @@ export default function Contributions() {
         </li>
 
         <li className="">
-          <a className="underline" href="https://gcc.gnu.org/git/?p=gcc.git;a=commitdiff;h=54bf72ebfe983c611673bcc4368b5834773fcc7d">c++: Fix ICE on mangling invalid compound requirement</a>
+          <a className="underline" href="https://forge.sourceware.org/gcc/gcc-mirror/commit/54bf72ebfe983c611673bcc4368b5834773fcc7d">c++: Fix ICE on mangling invalid compound requirement</a>
         </li>
 
         <ul className="list-disc ml-6">
@@ -118,6 +123,10 @@ export default function Contributions() {
             <a className="underline" href="https://gcc.gnu.org/bugzilla/show_bug.cgi?id=120618">Resolved bug 120618</a>
           </li>
         </ul>
+
+        <li>
+          <a className="underline" href="https://forge.sourceware.org/gcc/gcc-mirror/commit/52d702d72afa0ade8edfff144b45539495e4d408">libstdc++: fix element construction in std::deque::emplace</a>
+        </li>
 
       </ul>
 
