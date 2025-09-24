d9c6f04b9e63b41443a9ad4a432dff21c352dd36
benwu25 <soggysocks206@gmail.com>
2025-09-24 11:34:29 -0700

    add issues in progress

--- src/Contributions.tsx
+++ src/Contributions.tsx
@@ -102,6 +102,11 @@ export default function Contributions() {
             <a className="underline" href="https://github.com/llvm/llvm-project/issues/157524">Resolved issue 157524</a>
           </li>
         </ul>
+
+        <li>
+          <a className="underline" href="https://github.com/llvm/llvm-project/issues/159743">In progress: [MLIR][CF][Canonicalizer] infinite loop trying to simplify control flow</a>
+        </li>
+
       </ul>
 
       <br></br>
@@ -139,6 +144,10 @@ export default function Contributions() {
             <a className="underline" href="https://gcc.gnu.org/bugzilla/show_bug.cgi?id=118087">Resolved bug 118087</a>
           </li>
         </ul>
+
+        <li>
+          <a className="underline" href="https://gcc.gnu.org/bugzilla/show_bug.cgi?id=119745">In progress: [C++23] Implement P2438R2, basic_string::substr() &&</a>
+        </li>
 
       </ul>
 
