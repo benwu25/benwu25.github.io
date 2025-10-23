68b55fdde9ae135c4c871fda432e7c840caa6bb4
benwu25 <bwu25@cs.washington.edu>
2025-10-23 15:20:13 -0700

    adjust mobile

--- src/App.tsx
+++ src/App.tsx
@@ -66,7 +66,7 @@ export default function App() {
 
           <div>
             <a>
-              <img src={bin} className="h-50 -ml-[400%]"></img>
+              <img src={bin} className="h-50 -ml-[300%]"></img>
             </a>
           </div>
 
