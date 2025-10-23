c64097536a8d4650854f27dc298c2a67a30e0fcd
benwu25 <bwu25@cs.washington.edu>
2025-10-23 13:34:21 -0700

    Add profile image

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router'
 import { isMobile } from 'react-device-detect'
+import bin from '/src/assets/DSC01012.jpeg'
 import './App.css'
 
 export default function App() {
@@ -76,50 +77,66 @@ export default function App() {
       <br></br>
       <br></br>
 
-      <div>
-        <b className="ml-[6.65%] text-[40px]">Ben Wu</b>
+
+      <div className="ml-[6.65%] flex">
+        <div>
+
+          <div>
+            <b className="text-[40px]">Ben Wu</b>
+          </div>
+
+          <div>
+            <a className="underline ml-[0.2%]" href="mailto:bwu25@cs.washington.edu">Email</a>
+          </div>
+
+          <div>
+            <a className="underline ml-[0.2%]" href="https://www.linkedin.com/in/benwu25">LinkedIn</a>
+          </div>
+
+          <div>
+            <a className="underline ml-[0.2%]" href="https://github.com/benwu25">GitHub</a>
+          </div>
+
+          <div>
+            <a className="underline ml-[0.2%]" href="https://leetcode.com/u/bwu25/">LeetCode</a>
+          </div>
+
+          <br></br>
+          <br></br>
+          <br></br>
+
+          <div>
+            <b className="text-[20px] ml-[0.85%]">About:</b>
+          </div>
+
+          <div className="max-w-180 text-3x1 ml-[0.85%]">
+            Hi, I'm Ben. I am an undergraduate student at the University of Washington Seattle campus studying computer engineering in the Paul G. Allen School.
+          </div>
+
+          <br></br>
+
+          <div className="max-w-180 text-3x1 ml-[0.85%]">
+            Outside of school, I enjoy building, reading, using, and contributing to open source software. I am particularly interested in compiler toolchains, and I am spending a lot of my time learning and hacking inside of the LLVM project and the GNU Compiler Collection.
+          </div>
+
+          <br></br>
+          <br></br>
+
+          <div>
+            <Link className="underline ml-[0.85%]" to="/contributions">Open source contributions</Link>
+          </div>
+
+
+        </div>
+
+        <div>
+          <a>
+            <img src={bin} className="h-130 ml-[30%]"></img>
+          </a>
+        </div>
+
       </div>
 
-      <div>
-        <a className="underline ml-[6.85%]" href="mailto:bwu25@cs.washington.edu">Email</a>
-      </div>
-
-      <div>
-        <a className="underline ml-[6.85%]" href="https://www.linkedin.com/in/benwu25">LinkedIn</a>
-      </div>
-
-      <div>
-        <a className="underline ml-[6.85%]" href="https://github.com/benwu25">GitHub</a>
-      </div>
-
-      <div>
-        <a className="underline ml-[6.85%]" href="https://leetcode.com/u/bwu25/">LeetCode</a>
-      </div>
-
-      <br></br>
-      <br></br>
-      <br></br>
-
-      <div>
-        <b className="text-[20px] ml-[7.5%]">About:</b>
-      </div>
-
-      <div className="max-w-180 text-3x1 ml-[7.5%]">
-        Hi, I'm Ben. I am an undergraduate student at the University of Washington Seattle campus studying computer engineering in the Paul G. Allen School.
-      </div>
-
-      <br></br>
-
-      <div className="max-w-180 text-3x1 ml-[7.5%]">
-        Outside of school, I enjoy building, reading, using, and contributing to open source software. I am particularly interested in compiler toolchains, and I am spending a lot of my time learning and hacking inside of the LLVM project and the GNU Compiler Collection.
-      </div>
-
-      <br></br>
-      <br></br>
-
-      <div>
-        <Link className="underline ml-[7.5%]" to="/contributions">Open source contributions</Link>
-      </div>
 
       <br></br>
       <br></br>
